373757b70c41a240c873511ed561f704be0b387b
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-07-18 09:49:54 +0200

    #34 Refactor visualizer plugin

--- api/src/api/components/visualizer.py
+++ api/src/api/components/visualizer.py
@@ -6,8 +6,22 @@ from api.models.graph import Graph
 
 class VisualizerPlugin(Plugin):
     """
-    An abstraction representing a plugin for visualizing graph data. 
-    Its task is to output HTML string for a graph representation based on the given Graph object.
+    An abstraction representing a plugin for visualizing graph data.
+    Its task is to output an HTML string for a graph representation based on the given Graph object.
+
+    Plugins must follow integration rules:
+    - For interactivity, the following optional attributes can be added:
+        * enabled="true" - for elements that should be rendered
+        * drag="true" - for nodes that should be draggable
+        * tooltip="true" - for elements that should show tooltip on hover
+        * zoom="true" - for top-level <svg> or <g> elements that should support zoom/pan
+        * click-focus="true" - for elements that should respond to click focus
+
+    - Elements:
+        * nodes - tag: <g>, class='node'
+        * links - tag: <path>, class='link'
+
+    - All elements must be renderable in a single HTML string.
     """
 
     @abstractmethod
