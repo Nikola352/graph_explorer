5886a6d96503d391bdda852f03d14f4b31dd46c8
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-07-18 10:25:14 +0200

    #35 Add zoom and pan

--- graph_explorer/graph_explorer/static/js/main_view.script.js
+++ graph_explorer/graph_explorer/static/js/main_view.script.js
@@ -73,4 +73,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 d.fx = null;
                 d.fy = null;
         }));
+
+    var g = d3.select("svg[zoom='true'] g");
+    const handleZoom = (e) => g.attr('transform', e.transform);
+    const zoom = d3.zoom().on('zoom', handleZoom);
+    d3.selectAll("svg[zoom='true']").call(zoom);
 });
